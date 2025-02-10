54be34e2d2f887381d0aaa189e192f00d7e642a5
Diana <dianaoliveirandrade@gmail.com>
2025-02-10 11:15:26 -0300

    Add files via upload

---
+++ aula1/src/primeiraAula/Soma.java
@@ -0,0 +1,18 @@
+package primeiraAula;
+
+import java.util.Scanner;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int x, y, soma;
+		x = sc.nextInt();
+		y = sc.nextInt();
+		soma = (x + y);
+		System.out.println("soma:" + soma);
+	}
+
+}
