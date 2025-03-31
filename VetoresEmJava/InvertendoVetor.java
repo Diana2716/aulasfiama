8204c34f414589d754bcb88dc48ff076f70f6870
Diana <dianaoliveirandrade@gmail.com>
2025-03-31 11:07:25 -0300

    Update InvertendoVetor.java

--- VetoresEmJava/InvertendoVetor.java
+++ VetoresEmJava/InvertendoVetor.java
@@ -4,7 +4,19 @@ public class InvertendoVetor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	int[] vetor = {3, 7, 1, 9, 4, 2};
+		
+		System.out.println("Vetor original: ");
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println(vetor[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.println("Vetor invertido: ");
+		for (int i = vetor.length - 1; i >= 0; i--) {
+			System.out.println(vetor[i] + " ");
+		}
 	}
 
 }
+
